a79cfdf8fa04c9d9ba89908a9890489e2f00b62c
Gordon Ray <gray2884@gmail.com>
2019-07-02 16:48:29 -0500

    another try

--- src/server.js
+++ src/server.js
@@ -1,23 +1,39 @@
-const app = require("./app");
-const http = require("http");
-const port = normalizePort(process.env.PORT || "3001");
-const server = http.createServer(app);
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const appConfig = require("./src/config/main-config.js");
 
-app.set("port", port);
+const port = process.env.PORT || 3001;
 
-server.listen(port);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-function normalizePort(val) {
-  const port = parseInt(val, 10);
-  if (isNaN(port)) {
-    return val;
-  }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
+//initialize node app
+appConfig.init(app,express);
+
+// API calls
+const staticRoutes = require("./routes/static");
+const userRoutes = require("./routes/users");
+const listRoutes = require("./routes/lists");
+const itemRoutes = require("./routes/items");
+
+
+app.use('/',staticRoutes);
+app.use('/',userRoutes);
+app.use('/',listRoutes);
+app.use('/',itemRoutes);
+
+
+if (process.env.NODE_ENV === 'production') {
+
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '..client/build', 'index.html'));
+  });
 }
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
-server.on("listening", () => {
-  console.log(`server is listening for requests on port ${server.address().port}`);
-});
+module.exports = app;
