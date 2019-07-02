da1cc3545083e6a0711eb5a04d11b94da3ed2775
Gordon Ray <gray2884@gmail.com>
2019-07-02 16:30:08 -0500

    server file edits to push to heroku

--- src/server.js
+++ src/server.js
@@ -1,23 +1,33 @@
-const app = require("./app");
-const http = require("http");
-const port = normalizePort(process.env.PORT || "3001");
-const server = http.createServer(app);
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-app.set("port", port);
+const app = express();
+const port = process.env.PORT || 3001;
 
-server.listen(port);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-function normalizePort(val) {
-  const port = parseInt(val, 10);
-  if (isNaN(port)) {
-    return val;
-  }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
+// API calls
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
+
+app.post('/api/world', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
+});
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+    
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
 
-server.on("listening", () => {
-  console.log(`server is listening for requests on port ${server.address().port}`);
-});
+app.listen(port, () => console.log(`Listening on port ${port}`));
