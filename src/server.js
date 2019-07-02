127d1b30152dbe57e216e19e775cb4efcf0c33c8
Gordon Ray <gray2884@gmail.com>
2019-07-02 18:31:06 -0500

    removing staticRoute to get deploy to default to index.html

--- src/server.js
+++ src/server.js
@@ -13,13 +13,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 appConfig.init(app,express);
 
 // API calls
-const staticRoutes = require("./routes/static");
+// const staticRoutes = require("./routes/static");
 const userRoutes = require("./routes/users");
 const listRoutes = require("./routes/lists");
 const itemRoutes = require("./routes/items");
 
 
-app.use('/',staticRoutes);
+// app.use('/',staticRoutes);
 app.use('/',userRoutes);
 app.use('/',listRoutes);
 app.use('/',itemRoutes);
